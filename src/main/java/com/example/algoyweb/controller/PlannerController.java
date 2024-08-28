540b4fa671da11e2004601ece966d8eb18ee5d81
zoanna <zoanna5442@gmail.com>
2024-08-28 10:33:11 +0900

    nginx test

--- src/main/java/com/example/algoyweb/controller/PlannerController.java
+++ src/main/java/com/example/algoyweb/controller/PlannerController.java
@@ -15,12 +15,11 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/web/planner")
 public class PlannerController {
 
     private final PlannerService plannerService;
 
-    @GetMapping
+    @GetMapping("/planner")
     //@PostAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<List<PlannerDto>> getCalender(@RequestParam int year, @RequestParam int month) {
         List<PlannerDto> plannerDtoList = plannerService.getPlansMonth(year, month);
@@ -36,7 +35,7 @@ public class PlannerController {
   //      return ResponseEntity.status(HttpStatus.OK).body(plannerDtoList);
     //}
 
-    @GetMapping("/{id}")
+    @GetMapping("/planner/{id}")
     //@PostAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<PlannerDto> getPlan(@PathVariable Long id) {
         PlannerDto plannerDto = plannerService.getPlan(id);
@@ -44,19 +43,19 @@ public class PlannerController {
         return ResponseEntity.status(HttpStatus.OK).body(plannerDto);
     }
 
-    @GetMapping("/save-form")
+    @GetMapping("/planner/save-form")
     //@PostAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ModelAndView saveForm() {
         return new ModelAndView("planner/SaveForm");
     }
 
-    @GetMapping("/calender")
+    @GetMapping("/planner/calender")
     //@PostAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ModelAndView viewCalender() {
         return new ModelAndView("planner/PlannerMain");
     }
 
-    @PostMapping("/save")
+    @PostMapping("/planner/save")
     //@PostAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<PlannerDto> savePlan(@RequestBody PlannerDto plannerDto) {
         PlannerDto savedDto = plannerService.savePlan(plannerDto);
@@ -64,7 +63,7 @@ public class PlannerController {
         return ResponseEntity.status(HttpStatus.CREATED).body(savedDto);
     }
 
-    @PostMapping("/edit/{id}")
+    @PostMapping("/planner/edit/{id}")
     //@PostAuthorize("hasAnyRole('ROLE_USER', 'ROLE_ADMIN')")
     public ResponseEntity<PlannerDto> editPlan(@RequestBody PlannerDto plannerDto, @PathVariable Long id/*@AuthenticationPrincipal UserDetails userDetails*/) {
         PlannerDto updatedDto = plannerService.updatePlan(plannerDto, id, /*userDetails.getUsername()*/ "1234");
@@ -72,7 +71,7 @@ public class PlannerController {
         return ResponseEntity.status(HttpStatus.OK).body(updatedDto);
     }
 
-    @PostMapping("/delete/{id}")
+    @PostMapping("/planner/delete/{id}")
     //@PostAuthorize("hasAnyRole('ROLE_USER','ROLE_ADMIN')")
     public ResponseEntity<String> deletePlan(@PathVariable Long id/*@AuthenticationPrincipal UserDetails userDetails*/) {
         plannerService.deletePlan(id, /*userDetails.getUsername()*/ "1234");
