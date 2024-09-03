b211b4bcaa3e7ca2a2e24fac5a47571c34d04c40
duys2 <minigun5@naver.com>
2024-09-03 10:16:16 +0900

    Fix: 바인딩 오류 해결
    UserDto에 Setter 추가

--- src/main/java/com/example/algoyweb/model/dto/user/UserDto.java
+++ src/main/java/com/example/algoyweb/model/dto/user/UserDto.java
@@ -8,9 +8,13 @@ import com.example.algoyweb.model.entity.user.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Builder
 @Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class UserDto {
   private Long userId;
