31541695b571c015625866695a11a5df3d14697f
lenac115 <lenac115@naver.com>
2024-09-03 09:39:32 +0900

    test: Planner
    Planner 관련 테스트 생성, Planner에 테스트 이름과 사이트 이름 작성 칼럼 생성
    
    Closes: #72

--- src/main/java/com/example/algoyweb/repository/planner/PlannerRepository.java
+++ src/main/java/com/example/algoyweb/repository/planner/PlannerRepository.java
@@ -5,14 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface PlannerRepository extends JpaRepository<Planner, Long> {
 
     @Query("select p from Planner p where p.startAt >= :startTime and p.startAt <= :endTime")
-    List<Planner> findByMonth(LocalDateTime startTime, LocalDateTime endTime);
+    List<Planner> findByMonth(LocalDate startTime, LocalDate endTime);
 
     @Query("select p from Planner p where p.user.email = :username")
     List<Planner> findByUserEmail(String username);
--- src/main/java/com/example/algoyweb/util/ConvertUtils.java
+++ src/main/java/com/example/algoyweb/util/ConvertUtils.java
@@ -9,44 +9,50 @@ import java.time.LocalDateTime;
 
 public class ConvertUtils {
 
-    public static PlannerDto convertPlannerToDto(Planner planner) {
-        return PlannerDto.builder()
-                .id(planner.getId())
-                .title(planner.getTitle())
-                .endAt(planner.getEndAt())
-                .startAt(planner.getStartAt())
-                .content(planner.getContent())
-                .createAt(planner.getCreateAt())
-                .status(planner.getStatus())
-                .updateAt(planner.getUpdateAt())
-                .link(planner.getLink())
-                .build();
-    }
+  public static PlannerDto convertPlannerToDto(Planner planner) {
+    return PlannerDto.builder()
+        .id(planner.getId())
+        .title(planner.getTitle())
+        .endAt(planner.getEndAt())
+        .startAt(planner.getStartAt())
+        .content(planner.getContent())
+        .createAt(planner.getCreateAt())
+        .status(planner.getStatus())
+        .updateAt(planner.getUpdateAt())
+        .link(planner.getLink())
+        .questionName(planner.getQuestionName())
+            .etcName(planner.getEtcName())
+        .siteName(planner.getSiteName())
+        .build();
+  }
 
-    public static Planner convertDtoToPlanner(PlannerDto plannerDto) {
-        return Planner.builder()
-                .content(plannerDto.getContent())
-                .createAt(LocalDateTime.now())
-                .status(plannerDto.getStatus())
-                .updateAt(LocalDateTime.now())
-                .endAt(plannerDto.getEndAt())
-                .title(plannerDto.getTitle())
-                .startAt(plannerDto.getStartAt())
-                .link(plannerDto.getLink())
-                .build();
-    }
+  public static Planner convertDtoToPlanner(PlannerDto plannerDto) {
+    return Planner.builder()
+        .content(plannerDto.getContent())
+        .createAt(LocalDateTime.now())
+        .status(plannerDto.getStatus())
+        .updateAt(LocalDateTime.now())
+        .endAt(plannerDto.getEndAt())
+        .title(plannerDto.getTitle())
+        .startAt(plannerDto.getStartAt())
+        .link(plannerDto.getLink())
+        .siteName(plannerDto.getSiteName())
+            .etcName(plannerDto.getEtcName())
+        .questionName(plannerDto.getQuestionName())
+        .build();
+  }
 
-    public static UserDto convertUserToDto(User findUser) {
-        return UserDto.builder()
-            .role(findUser.getRole())
-            .username(findUser.getUsername())
-            .email(findUser.getEmail())
-            .nickname(findUser.getNickname())
-            .userId(findUser.getUserId())
-            .isDeleted(findUser.getIsDeleted())
-            .createdAt(findUser.getCreatedAt())
-            .updatedAt(findUser.getUpdatedAt())
-            .deletedAt(findUser.getDeletedAt())
-            .build();
-    }
+  public static UserDto convertUserToDto(User findUser) {
+    return UserDto.builder()
+        .role(findUser.getRole())
+        .username(findUser.getUsername())
+        .email(findUser.getEmail())
+        .nickname(findUser.getNickname())
+        .userId(findUser.getUserId())
+        .isDeleted(findUser.getIsDeleted())
+        .createdAt(findUser.getCreatedAt())
+        .updatedAt(findUser.getUpdatedAt())
+        .deletedAt(findUser.getDeletedAt())
+        .build();
+  }
 }
