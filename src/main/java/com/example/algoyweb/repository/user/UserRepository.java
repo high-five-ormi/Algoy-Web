0a65c75ee9d6b12b05543379548793e1233133a9
duys2 <minigun5@naver.com>
2024-09-06 11:36:56 +0900

    Style: 코드 포매팅

--- src/main/java/com/example/algoyweb/repository/user/UserRepository.java
+++ src/main/java/com/example/algoyweb/repository/user/UserRepository.java
@@ -24,6 +24,5 @@ public interface UserRepository extends JpaRepository<User, Long> {
 	// 주어진 토큰으로 사용자를 조회 후 Optional로 반환하는 쿼리 메서드
 	Optional<User> findByPassword(String token); // 토큰이 임시 비밀번호로 저장되었으므로 이를 통해 찾기
 
-
 	List<User> findByIsDeletedTrueAndDeletedAtBefore(LocalDateTime now);
 }
