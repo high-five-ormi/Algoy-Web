96e9687094a8c03033fc3a8b2bda3d7b4b2f7fbb
JEESW <jeesw1221@gmail.com>
2024-09-07 16:23:06 +0900

    Design: 오답 노트 상세 보기 화면에 챗봇 프래그먼트 붙이기

--- src/main/java/com/example/algoyweb/controller/WrongAnswerNote/WrongAnswerNoteThymeleafController.java
+++ src/main/java/com/example/algoyweb/controller/WrongAnswerNote/WrongAnswerNoteThymeleafController.java
@@ -36,6 +36,7 @@ public class WrongAnswerNoteThymeleafController {
         Optional<WrongAnswerNoteDTO> note = service.findById(id);
         if (note.isPresent()) {
             model.addAttribute("note", note.get());
+            model.addAttribute("backendUrl", backendUrl);
             return "wronganswernote/view-wrong-answer-note";
         } else {
             return "error";
