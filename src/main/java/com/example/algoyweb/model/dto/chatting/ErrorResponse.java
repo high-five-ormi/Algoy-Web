7c38e8b183d821966bde23d962f10cf5ae64e38d
JEESW <jeesw1221@gmail.com>
2024-09-10 10:27:51 +0900

    Enhancement: 채팅에 글자 수 예외 처리 기능 추가

---
+++ src/main/java/com/example/algoyweb/model/dto/chatting/ErrorResponse.java
@@ -0,0 +1,13 @@
+package com.example.algoyweb.model.dto.chatting;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorResponse {
+
+	private String message;
+}
